faaee19efff4361fe893e1c7a91301cec14b9432
Felipe Silva <amorimdub@gmail.com>
2020-10-06 11:17:59 +0100

    feat(image-filter): add image filter feature

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -15,11 +15,11 @@ import { V0MODELS } from './controllers/v0/model.index';
   await sequelize.sync();
 
   const app = express();
-  const port = process.env.PORT || 8080; // default port to listen
+  const port = process.env.PORT || 8080;
   app.use(bodyParser.json());
 
   app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8100");
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   });
